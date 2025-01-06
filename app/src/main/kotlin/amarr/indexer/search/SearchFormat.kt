2e6f8acd26e050de26f9b9a07d818ddb1b51b502
Dani <danielchenelcea@gmail.com>
2025-01-06 19:28:31 +0100

    feat(search) Added new TV search format

--- app/src/main/kotlin/amarr/indexer/search/SearchFormat.kt
+++ app/src/main/kotlin/amarr/indexer/search/SearchFormat.kt
@@ -2,6 +2,6 @@ package amarr.indexer.search
 
 class SearchFormat {
     companion object {
-        val tvSearchFormat: List<String> = listOf("%s %dx%02d", "%s S%02dE%02d")
+        val tvSearchFormat: List<String> = listOf("%s %dx%02d", "%s S%02dE%02d", "%s %d%02d")
     }
 }
